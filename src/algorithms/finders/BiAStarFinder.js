0110ba0e4312c1b12bf5595fd265d72a73e0779f
golebiowskib <bartosz.golebiowski@@ttpsc.pl>
2021-08-15 14:41:16 +0200

    last fixies

--- src/algorithms/finders/BiAStarFinder.js
+++ src/algorithms/finders/BiAStarFinder.js
@@ -1,25 +1,109 @@
-var AStarFinder = require('./AStarFinder');
+var Heap = require("heap");
+var Util = require("../core/Util");
+var Heuristic = require("../core/Heuristic");
+var DiagonalMovement = require("../core/DiagonalMovement");
 
-/**
- * Best-First-Search path-finder.
- * @constructor
- * @extends AStarFinder
- * @param {Object} opt
- * @param {boolean} opt.allowDiagonal Whether diagonal movement is allowed.
- *     Deprecated, use diagonalMovement instead.
- * @param {boolean} opt.dontCrossCorners Disallow diagonal movement touching
- *     block corners. Deprecated, use diagonalMovement instead.
- * @param {DiagonalMovement} opt.diagonalMovement Allowed diagonal movement.
- * @param {function} opt.heuristic Heuristic function to estimate the distance
- *     (defaults to manhattan).
- */
+function AStarFinder(opt) {
+  opt = opt || {};
+  this.allowDiagonal = opt.allowDiagonal;
+  this.dontCrossCorners = opt.dontCrossCorners;
+  this.heuristic = opt.heuristic || Heuristic.manhattan;
+  this.weight = opt.weight || 1;
+  this.diagonalMovement = opt.diagonalMovement;
+
+  if (!this.diagonalMovement) {
+    if (!this.allowDiagonal) {
+      this.diagonalMovement = DiagonalMovement.Never;
+    } else {
+      if (this.dontCrossCorners) {
+        this.diagonalMovement = DiagonalMovement.OnlyWhenNoObstacles;
+      } else {
+        this.diagonalMovement = DiagonalMovement.IfAtMostOneObstacle;
+      }
+    }
+  }
+
+  if (this.diagonalMovement === DiagonalMovement.Never) {
+    this.heuristic = opt.heuristic || Heuristic.manhattan;
+  } else {
+    this.heuristic = opt.heuristic || Heuristic.octile;
+  }
+}
+
+AStarFinder.prototype.findPath = function (startX, startY, endX, endY, grid) {
+  var openList = new Heap(function (nodeA, nodeB) {
+      return nodeA.f - nodeB.f;
+    }),
+    startNode = grid.getNodeAt(startX, startY),
+    endNode = grid.getNodeAt(endX, endY),
+    heuristic = this.heuristic,
+    diagonalMovement = this.diagonalMovement,
+    weight = this.weight,
+    abs = Math.abs,
+    SQRT2 = Math.SQRT2,
+    node,
+    neighbors,
+    neighbor,
+    i,
+    l,
+    x,
+    y,
+    ng;
+
+  startNode.g = 0;
+  startNode.f = 0;
+
+  openList.push(startNode);
+  startNode.opened = true;
+
+  while (!openList.empty()) {
+    node = openList.pop();
+    node.closed = true;
+
+    if (node === endNode) {
+      return Util.backtrace(endNode);
+    }
+
+    neighbors = grid.getNeighbors(node, diagonalMovement);
+    for (i = 0, l = neighbors.length; i < l; ++i) {
+      neighbor = neighbors[i];
+
+      if (neighbor.closed) {
+        continue;
+      }
+
+      x = neighbor.x;
+      y = neighbor.y;
+
+      ng = node.g + (x - node.x === 0 || y - node.y === 0 ? 1 : SQRT2);
+
+      if (!neighbor.opened || ng < neighbor.g) {
+        neighbor.g = ng;
+        neighbor.h =
+          neighbor.h || weight * heuristic(abs(x - endX), abs(y - endY));
+        neighbor.f = neighbor.g + neighbor.h;
+        neighbor.parent = node;
+
+        if (!neighbor.opened) {
+          openList.push(neighbor);
+          neighbor.opened = true;
+        } else {
+          openList.updateItem(neighbor);
+        }
+      }
+    }
+  }
+
+  return [];
+};
+
 function BestFirstFinder(opt) {
-    AStarFinder.call(this, opt);
+  AStarFinder.call(this, opt);
 
-    var orig = this.heuristic;
-    this.heuristic = function(dx, dy) {
-        return orig(dx, dy) * 1000000;
-    };
+  var orig = this.heuristic;
+  this.heuristic = function (dx, dy) {
+    return orig(dx, dy) * 1000000;
+  };
 }
 
 BestFirstFinder.prototype = new AStarFinder();
--- src/config/contants.js
+++ src/config/contants.js
@@ -4,53 +4,16 @@ export const MESH_HEIGTH = 1024;
 export const ITERATION_STEP = 2;
 export const AHEAD_PREDICTION_STEPS = 6;
 
-const getRandomArbitrary = (min, max)=> {
-  return Math.round(Math.random() * (max - min) + min);
-}
-const OFFSET = ()=> getRandomArbitrary(-2,3)
+const generateCoooridnates = () => {
+  const START = 1;
+  const END = 100;
+  const COORDINATES = [];
+  for (let index = 1; index < 100; index++) {
+    COORDINATES.push([START * index, START, END - START * index, END]);
+    COORDINATES.push([END - START * index, END, START * index, START]);
+  }
+  return COORDINATES;
+};
 
-
-export const START_X = 5;
-export const START_Y = 5;
-export const END_X = 88;
-export const END_Y = 88;
-export const START_X2 = 10;
-export const START_Y2 = 35;
-export const END_X2 = 55;
-export const END_Y2 = 66;
-export const START_X3 = 77;
-export const START_Y3 = 45;
-export const END_X3 = 10;
-export const END_Y3 = 66;
-export const START_X4 = 31;
-export const START_Y4 = 86;
-export const END_X4 = 5;
-export const END_Y4 = 8;
-
-
-export const COORDINATES = [
-  [START_X, START_Y, END_X, END_Y],
-  [END_X, END_Y, START_X, START_Y],
-  [START_X2, START_Y2, END_X2, END_Y2],
-  [END_X2, END_Y2, START_X2, START_Y2],
-  [START_X3, START_Y3, END_X3, END_Y3],
-  [END_X3, END_Y3, START_X3, START_Y3],
-  [START_X4, START_Y4, END_X4, END_Y4],
-  [END_X4, END_Y4, START_X4, START_Y4],
-];
-
-
+export const COORDINATES = generateCoooridnates();
 export const CARS_QUANTITY = COORDINATES.length;
-// const generateCoooridnates = ()=>{
-//   const START = 1;
-//   const END = 100;
-// const COORDINATES = []
-// for (let index = 1; index < 100; index++) {
-//   COORDINATES.push([START * index, START, END - START * index, END])
-//   COORDINATES.push([END - START * index, END, START * index, START])
-// }
-// return COORDINATES
-// }
-
-// export const COORDINATES = generateCoooridnates()
-// export const CARS_QUANTITY = COORDINATES.length;
